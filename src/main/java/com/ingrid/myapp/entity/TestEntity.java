9b9df99cfc58aa0780ebd3d61959f625f96e6a07
ingridcruzy <ingrid2209c@gmail.com>
2023-09-21 21:02:06 -0500

    Nombre del proyecto: my-app, Descripción: vrsión final del proyecto my-app

---
+++ src/main/java/com/ingrid/myapp/entity/TestEntity.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package com.ingrid.myapp.entity;
+
+/**
+ * @author INGRID
+ */
+public class TestEntity {
+	private String nombre;
+
+	/**
+	 * @return the nombre
+	 */
+	public String getNombre() {
+		return nombre;
+	}
+
+	/**
+	 * @param nombre the nombre to set
+	 */
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+}
